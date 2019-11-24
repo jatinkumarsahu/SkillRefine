7c6936079a23be2b0deb4f7790b206ed862a9c93
JatinPC <jatinkumar.sahu619@gmail.com>
2019-11-24 22:54:19 +0530

    First Commit PC

---
+++ src/main/java/com/jks/SkillRefineApplication.java
@@ -0,0 +1,13 @@
+package com.jks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillRefineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillRefineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jks/SkillRefineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillRefineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
