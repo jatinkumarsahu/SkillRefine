86ae6610f1265c2688d08ef180af1bb5982cc937
JatinPC <jatinkumar.sahu619@gmail.com>
2019-11-24 22:57:53 +0530

    Second Commit

--- src/main/java/com/jks/SkillRefineApplication.java
+++ src/main/java/com/jks/SkillRefineApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkillRefineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkillRefineApplication.class, args);
+		SpringApplication.run(SkillRefineApplication.class, args); 
 	}
 
 }
